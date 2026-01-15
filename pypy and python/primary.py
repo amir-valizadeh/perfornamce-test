89ff5e47b4aa243e73ac4e44e2ad11a745b85166
amir valizadeh <amirvalizadeh8731@gmail.com>
2026-01-15 15:20:00 +0400

    refactor python logic and add tests

--- pypy and python/primary.py
+++ pypy and python/primary.py
@@ -1,15 +1,14 @@
-#both python and pypy have a same syntax
+def count_primes(limit):
+    count = 0
+    for c in range(2, limit):
+        is_prime = True
+        for inner_c in range(2, int(c / 2 + 1)):
+            if c % inner_c == 0:
+                is_prime = False
+                break
+        if is_prime:
+            count += 1
+    return count
 
-count = 0
-is_prime = False
-for c in range(2, 3_00_000):
-    is_prime = True
-    for inner_c in range(2, int(c / 2 + 1)):
-        if c % inner_c == 0:
-            is_prime = False
-            break
-
-    if is_prime:
-        count += 1
-
-print(count)
+if __name__ == '__main__':
+    print(count_primes(300_000))
---
+++ pypy and python/test_primary.py
@@ -0,0 +1,11 @@
+import unittest
+from primary import count_primes
+
+class TestPrimary(unittest.TestCase):
+    def test_count_primes(self):
+        self.assertEqual(count_primes(10), 4)
+        self.assertEqual(count_primes(20), 8)
+        self.assertEqual(count_primes(100), 25)
+
+if __name__ == '__main__':
+    unittest.main()
