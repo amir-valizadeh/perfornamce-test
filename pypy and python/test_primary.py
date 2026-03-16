f2663addca40c7ffead3f50520588bbbde7b7d0b
amir valizadeh <amirvalizadeh8731@gmail.com>
2026-03-16 12:00:00 +0400

    add edge case tests for python

--- pypy and python/test_primary.py
+++ pypy and python/test_primary.py
@@ -3,6 +3,8 @@ from primary import count_primes
 
 class TestPrimary(unittest.TestCase):
     def test_count_primes(self):
+        self.assertEqual(count_primes(0), 0)
+        self.assertEqual(count_primes(2), 0)
         self.assertEqual(count_primes(10), 4)
         self.assertEqual(count_primes(20), 8)
         self.assertEqual(count_primes(100), 25)
