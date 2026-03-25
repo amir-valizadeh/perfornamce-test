9d2874d4cacb43131398de6eaead228dd4be2aae
amir valizadeh <amirvalizadeh8731@gmail.com>
2026-03-25 15:00:00 +0400

    add java documentation to count primes

--- java/Main.java
+++ java/Main.java
@@ -1,4 +1,10 @@
 public class Main {
+    /**
+     * Counts prime numbers up to the given limit using O(n^2) complexity.
+     *
+     * @param limit The upper bound to check.
+     * @return The number of prime numbers found.
+     */
     public static int countPrimes(int limit) {
         int count = 0;
         for (int i = 2; i < limit; i++) {
